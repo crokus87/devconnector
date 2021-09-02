b099bf8dd1dd639a8fdced6b6f553d9579d81cde
Crokus87 <j.reist@outlook.com>
2021-09-02 16:46:37 -0600

    fixed issue with Navbar not correctly loading at launch

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -37,9 +37,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
           <i className="fas fa-code"></i> DevConnector
         </Link>
       </h1>
-      {!loading && (
-        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
-      )}
+      <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
     </nav>
   );
 };
